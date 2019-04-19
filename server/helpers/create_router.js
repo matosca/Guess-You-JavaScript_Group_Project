bf2c8046e7b040dfe14a1b1617e4a17979cfc5af
Mark <nirvanafoo86@hotmail.com>
2019-04-19 15:25:18 +0100

    Created get method inside of router

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -3,6 +3,16 @@ const ObjectId = require('mongodb').ObjectId
 
 const createRouter = function (collection) {
  const router = express.Router();
+
+ router.get ('/', (req, res) => {
+   collection
+     .find().toArray();
+     .then( (docs) => res.json(docs) )
+     .catch(err)
+     console.error(err);
+     res.status(500);
+     res.json({ status: 500, error: err});
+ });
 };
 
 module.exports = createRouter;
