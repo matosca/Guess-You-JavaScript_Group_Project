b9eac9fc3da09d8af85ba5acbf934d62b358eabd
user <user@users-mbp-5.home>
2019-04-19 18:04:03 +0100

    seed files updated

---
+++ server/db/seeds_characters.js
@@ -0,0 +1,138 @@
+use guesswho;
+db.dropDatabase();
+
+db.characters.insertMany(
+[
+  {
+  name: 'Anita',
+  inplay: true,
+  image_url:,
+  hair_colour: 'blonde',
+  eye_colour: 'blue',
+  gender: 'f',
+  hat: false,
+  glasses: false,
+  facial_hair: false
+},
+{
+  name: 'Robert',
+  inplay: true,
+  image_url: ,
+  hair_colour: 'brown',
+  eye_colour: 'blue',
+  gender: 'm',
+  hat: false,
+  glasses: false,
+  facial_hair: false
+},
+{
+  name: 'George',
+  inplay: true,
+  image_url:,
+  hair_colour: 'white',
+  eye_colour: 'brown',
+  gender: 'm',
+  hat: true,
+  glasses: false,
+  facial_hair: false
+},
+{
+  name: 'Paul',
+  inplay: true,
+  image_url:,
+  hair_colour: 'white',
+  eye_colour: 'brown',
+  gender: 'm',
+  hat: false,
+  glasses: true,
+  facial_hair: false
+},
+{
+  name: 'Claire',
+  inplay: true,
+  image_url:,
+  hair_colour: 'red',
+  eye_colour: 'brown',
+  gender: 'f',
+  hat: true,
+  glasses: true,
+  facial_hair: false
+},
+{
+  name: 'Alfred',
+  inplay: true,
+  image_url:,
+  hair_colour: 'red',
+  eye_colour: 'blue',
+  gender: 'm',
+  hat: false,
+  glasses: false,
+  facial_hair: true
+},
+{
+  name: 'Susan',
+  inplay: true,
+  image_url:,
+  hair_colour: 'white',
+  eye_colour: 'brown',
+  gender: 'f',
+  hat: false,
+  glasses: false,
+  facial_hair: false
+},
+{
+  name: 'Maria',
+  inplay: true,
+  image_url:,
+  hair_colour: 'brown',
+  eye_colour: 'brown',
+  gender: 'f',
+  hat: true,
+  glasses: false,
+  facial_hair: false
+},
+{
+  name: 'David',
+  inplay: true,
+  image_url:,
+  hair_colour: 'blonde',
+  eye_colour: 'brown',
+  gender: 'm',
+  hat: false,
+  glasses: false,
+  facial_hair: true
+},
+{
+  name: 'Eric',
+  inplay: true,
+  image_url:,
+  hair_colour: 'blonde',
+  eye_colour: 'brown',
+  gender: 'm',
+  hat: true,
+  glasses: false,
+  facial_hair: false
+},
+{
+  name: 'Herman',
+  inplay: true,
+  image_url:,
+  hair_colour: 'red',
+  eye_colour: 'brown',
+  gender: 'm',
+  hat: false,
+  glasses: false,
+  facial_hair: false
+},
+{
+  name: 'Anne',
+  inplay: true,
+  image_url:,
+  hair_colour: 'brown',
+  eye_colour: 'brown',
+  gender: 'f',
+  hat: false,
+  glasses: false,
+  facial_hair: false
+}
+]);
