051502ddd0c999639f873d5c510ed0a1924de91f
Amy Edwardson <am4zed11@gmail.com>
2019-04-19 12:59:25 +0100

    Working on getData function

--- client/src/models/guess_who.js
+++ client/src/models/guess_who.js
@@ -4,6 +4,9 @@ const RequestHelper = require("../helpers/request_helper.js");
 const GuessWho = function(url){
   this.url = url;
   this.request = new RequestHelper(this.url);
+  this.gameData = null;
+  this.allQuestions = null;
+  this.characters = null;
 };
 
 GuessWho.prototype.bindEvents = function () {
@@ -11,3 +14,34 @@ GuessWho.prototype.bindEvents = function () {
     this.getResult(evt.detail);
   });
 };
+
+GuessWho.prototype.getData = function () {
+  this.request
+  .get()
+  .then((gameData) => {
+    this.gameData = gameData;
+    this.allQuestions = gameData.questions;
+    this.character = gameData.characters;
+  }
+    questions => {
+    PubSub.publish("GuessWho:all-questions-ready", questions);
+  })
+  .catch(console.error);
+};
+
+GuessWho.prototype.getResult = function (questionId) {
+  this.getCardsToEliminate(questionId)
+};
+
+GuessWho.prototype.getCardsToEliminate = function (questionId) {
+  const cardsToEliminate = []
+  const questions =
+  for (question in questions) {
+    if questioniD === question.id
+
+  };
+};
+
+
+
+module.exports = GuessWho;
