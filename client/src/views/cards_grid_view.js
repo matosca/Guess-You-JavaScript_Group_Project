f30d1dfda24536c5e2166041e45686bb63957030
marisol <mtoscanor37@gmail.com>
2019-04-19 17:43:30 +0100

    Finished grid view for displaying cards

--- client/src/views/cards_grid_view.js
+++ client/src/views/cards_grid_view.js
@@ -11,6 +11,21 @@ CardsGridView.prototype.bindEvents = function () {
   });
 };
 
-CardsGridView.prototype.render = function () {
-  
-}
+CardsGridView.prototype.render = function (charactersData) {
+  charactersData.forEach( (card) => {
+    const cardContainer = this.createCards(card); //this creates every card in the format from below
+    this.container.appendChild(cardContainer); //appending each card into the main container
+  });
+};
+
+
+CardsGridView.prototype.createCards = function (card) {
+  const cardContainer = document.createElement('div'); //creating container for allocating the img
+  cardContainer.classList.add('card');
+
+  const characterImg = document.createElement('img');//creating tag img in html
+  characterImg.src = card.image_url;//setting the src to be the card's url from the database
+
+  cardContainer.appendChild(characterImg); //appending the img into the container
+  return cardContainer;
+};
