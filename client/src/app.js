48df8296a860205c690e3398b45c0fdfd048fcb6
Amy Edwardson <am4zed11@gmail.com>
2019-04-19 21:33:50 +0100

    Built app

--- client/src/app.js
+++ client/src/app.js
@@ -3,7 +3,20 @@ const SelectView = require('.views/select_view.js');
 const CardsGridView = require('./views/cards_grid_view.js');
 const GameResultView = require('./views/game_result_view.js');
 
-const url = "http://localhost:3000";
-  const activities = new BucketList(url);
-  activities.bindEvents();
-  activities.getData();
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const questionSelectForm = document.querySelector('form#select-form');
+  const selectView = new SelectView(questionSelectForm);
+  selectView.bindEvents();
+
+  const characterGridDiv = document.querySelector('div#character-grid');
+  const cardsGridView = new CardsGridView(characterGridDiv);
+  cardsGridView.bindEvents();
+
+  const url = "http://localhost:3000";
+  const newGame = new GuessWho(url);
+  newGame.bindEvents();
+  newGame.getData();
+  
+});
