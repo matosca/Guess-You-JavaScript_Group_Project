19a77fd6b0d6c9ffa828cf3a8b396f37d8475037
Amy Edwardson <am4zed11@gmail.com>
2019-04-19 20:31:45 +0100

    Finished getCardsToEliminate and updateCards functions - still untested. Started work on app

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,7 @@
-
+const GuessWho = require('./models/guess_who.js');
+const SelectView = require('.views/select_view.js');
+const CardsGridView = require('./views/cards_grid_view.js');
+const GameResultView = require('./views/game_result_view.js');
 
 const url = "http://localhost:3000";
   const activities = new BucketList(url);
