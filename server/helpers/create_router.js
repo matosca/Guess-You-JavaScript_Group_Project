3a11d345bdc1a6e056160e3096afa59a246e3b87
Mark <nirvanafoo86@hotmail.com>
2019-04-19 14:42:08 +0100

    Creating router

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -3,6 +3,8 @@ const ObjectId = require('mongodb').ObjectId
 
 const createRouter = function (collection) {
  const router = express.Router();
+
+ get '/' (res,req)
 };
 
 module.exports = createRouter;
