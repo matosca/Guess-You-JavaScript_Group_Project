9f76f08936d70b1d54ac481e2d3bd9c63160a904
Mark <nirvanafoo86@hotmail.com>
2019-04-19 14:12:42 +0100

    Creating the createRouter function

---
+++ server/helpers/create_router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const ObjectId = require('mongodb').ObjectId
+
+const createRouter = function (collection) {
+ const router = express.Router();
+};
+
+module.exports = createRouter;
