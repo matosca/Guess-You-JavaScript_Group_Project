4305e0f50a5c132ee4e2be7b361f34f915b4e5b5
user <user@users-MacBook-Pro-5.local>
2019-04-20 15:44:12 +0100

    updated questions to have all standard questions and one non standard. added character values to reflect this.

--- server/db/seeds_questions.js
+++ server/db/seeds_questions.js
@@ -37,5 +37,46 @@ id: 6
 question: "Do they have Brown Eyes?"
 related_key: "eye_colour"
 attribute: "brown"
+},
+{
+id: 6
+question: "Do they have Brown Eyes?"
+related_key: "eye_colour"
+attribute: "brown"
+},{
+id: 7
+question: "Are they Female?"
+related_key: "gender"
+attribute: "f"
+},
+{
+id: 8
+question: "Are they Male?"
+related_key: "gender"
+attribute: "m"
+},
+{
+id: 9
+question: "Are they Wearing a Hat?"
+related_key: "hat"
+attribute: "true"
+},
+{
+id: 10
+question: "Are they Wearing Glasses?"
+related_key: "glasses"
+attribute: "true"
+},
+{
+id: 11
+question: "Do they have Facial Hair?"
+related_key: "facial_hair"
+attribute: "true"
+},
+{
+id: 12
+question: "Do they look like they are Disappointment to their Father?"
+related_key: "daddy_disappointed"
+attribute: "true"
 }
 ]);
