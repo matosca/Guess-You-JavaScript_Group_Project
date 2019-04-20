a22b1acd45475b38e94d70acdf94b6e96117d759
Mark <nirvanafoo86@hotmail.com>
2019-04-20 20:49:06 +0100

    commiting seeds, game_result_view and index.html

---
+++ client/src/views/game_result_view.js
@@ -0,0 +1,35 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const GameResultView = function (container) {
+  this.container = container;
+};
+
+GameResultView.prototype.bindEvents = function () {
+  PubSub.subscribe('GuessWho:guessed-card-result', (evt) => {
+    const resultedCard = evt.detail;
+    this.renderResult(resultedCard);
+  });
+};
+
+GameResultView.prototype.renderResult = function (guessedCard) {
+
+  const resultMessage = this.createElement('h2', `Congratulations! You Guessed ${guessedCard.name}`);
+  this.container.appendChild(resultMessage);
+
+  const resultBox = document.createElement('div');
+  resultBox.classList.add('result');
+
+  const resultCharacter = document.createElement('img');
+  resultCharacter.src = guessedCard.image_url;
+  resultBox.appendChild(resultCharacter);
+
+  this.container.appendChild(resultBox);
+};
+
+GameResultView.prototype.createElement = function (elementType, text) {
+  const element = document.createElement(elementType);
+  element.textContent = text;
+  return element;
+};
+
+module.exports = GameResultView;
--- server/db/seeds_questions.js
+++ server/db/seeds_questions.js
@@ -1,6 +1,7 @@
 use guesswho;
 
 db.questions.insertMany(
+<<<<<<< HEAD
 [
   {
   id: 1,
@@ -80,3 +81,85 @@ related_key: "daddy_disappointed"
 attribute: "true"
 }
 ]);
+=======
+  [
+    {
+      id: 0,
+      question: "Do they have Brunette Hair?",
+      related_key: "hair_colour",
+      attribute: "brown"
+    },
+    {
+      id: 1,
+      question: "Do they have Blonde Hair?",
+      related_key: "hair_colour",
+      attribute: "blonde"
+    },
+    {
+      id: 2,
+      question: "Do they have Red Hair?",
+      related_key: "hair_colour",
+      attribute: "red"
+    },
+    {
+      id: 3,
+      question: "Do they have White Hair,
+      related_key: "hair_colour",
+      attribute: "white"
+    },
+    {
+      id: 4,
+      question: "Do they have Blue Eyes,
+      related_key: "eye_colour",
+      attribute: "blue"
+    },
+    {
+      id: 5,
+      question: "Do they have Brown Eyes?",
+      related_key: "eye_colour",
+      attribute: "brown"
+    },
+    {
+      id: 6,
+      question: "Do they have Brown Eyes?",
+      related_key: "eye_colour",
+      attribute: "brown"
+    },{
+      id: 7,
+      question: "Are they Female?",
+      related_key: "gender",
+      attribute: "f"
+    },
+    {
+      id: 8,
+      question: "Are they Male?",
+      related_key: "gender",
+      attribute: "m"
+    },
+    {
+      id: 9,
+      question: "Are they Wearing a Hat?",
+      related_key: "hat",
+      attribute: "true"
+    },
+    {
+      id: 10,
+      question: "Are they Wearing Glasses?",
+      related_key: "glasses",
+      attribute: "true"
+    },
+    {
+      id: 11,
+      question: "Do they have Facial Hair?",
+      related_key: "facial_hair",
+      attribute: "true"
+    },
+    {
+      id: 12,
+      question: "Do they look like they are Disappointment to their Father?",
+      related_key: "daddy_disappointed",
+      attribute: "true"
+    }
+  ]
+);
+>>>>>>> f6cf723f145c35291ef8ab212ca286c224466e83
