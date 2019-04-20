8907383d4d2346f018e8b54583f5b51e440bf487
marisol <mtoscanor37@gmail.com>
2019-04-20 21:44:32 +0100

    Modified server to access two tables in db

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-const path =  require('path');
-const bodyParser =  require('body-parser');
-const express = require('express')
+const express = require('express');
 const app = express();
+const path = require('path');
+const parser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
 
@@ -12,12 +12,26 @@ app.use(parser.json());
 
 MongoClient.connect('mongodb://localhost:27017')
   .then((client) => {
-    const db = client.db('guesswho');
-    const guesswhoCollection = db.collection('guesswho');
-    const guesswhoRouter = db.createRouter(guesswhoCollection);
-    app.use('/api/guesswho', guesswhoRouter);
+    const db = client.db('guessWho'); //accessing the database
+
+    //accessing the collection of characters from 'guessWho' database
+    const charactersCollection = db.collection('characters');
+    //creating router for collection of characters
+    const charactersRouter = db.createRouter(charactersCollection);
+    app.use('/api/characters', charactersRouter);
+
+    //accessing the collectin of questions from 'guessWho' database
+    const questionsCollection = db.collection('questions');
+    //creating router for collection of questions
+    const questionsRouter = db.createRouter(questionsCollection);
+    app.use('/api/questions', questionsRouter);
+  })
+  .catch((err) => {
+    console.error('Failed to connect to DB');
+    console.error(err);
   });
 
+
   app.listen(3000, function () {
-  console.log(`Listening on Port ${ this.address().port}`);
+    console.log(`Listening on port ${ this.address().port }`);
 });
