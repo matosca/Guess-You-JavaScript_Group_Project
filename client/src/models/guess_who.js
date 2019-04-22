947bd4573b0ca63edee8d12f47df8a465520fa5f
Amy Edwardson <am4zed11@gmail.com>
2019-04-22 12:00:12 +0100

    getHiddenCharacter refactoring

--- client/src/models/guess_who.js
+++ client/src/models/guess_who.js
@@ -6,13 +6,14 @@ const GuessWho = function(category, url){
   this.url = url;
   this.request = new RequestHelper(this.url);
   this.gameData = null;
-  // this.allQuestions = null;
-  // this.characters = null;
+  this.allQuestions = null;
+  this.characters = null;
   this.hiddenCharacter = null;
 };
 
 GuessWho.prototype.bindEvents = function () {
   PubSub.subscribe('SelectView:question-selected', (evt) => {
+    console.log( 'this is the quetion', evt.detail);
     this.getResult(evt.detail);
   });
 };
@@ -23,16 +24,21 @@ GuessWho.prototype.getData = function () {
     this.gameData = gameData;
     console.log('Boo!', gameData);
     PubSub.publish(`GuessWho:${this.category}-data-loaded`, gameData);
-    if (this.category === 'characters'){
-      const hiddenCharacter = this.getHiddenCharacter(gameData);
-      this.hiddenCharacter = hiddenCharacter;
+    if (this.category === 'questions'){
+      this.allQuestions = gameData;
     }
+    else if (this.category === 'characters'){
+      this.characters = gameData;
+      const hiddenCharacter = this.getHiddenCharacter();
+      this.hiddenCharacter = hiddenCharacter;
+    };
   })
   .catch( (err) => console.error(err) );
 };
 
-GuessWho.prototype.getResult = function (questionId) {
-  const selectedQuestion = this.getSelectedQuestion(questionId);
+GuessWho.prototype.getResult = function (questionContent) {
+  const selectedQuestion = this.getSelectedQuestion(questionContent);
+  console.log('Hello!', selectedQuestion);
   const relatedKey = selectedQuestion.related_key;
   const attribute = selectedQuestion.attribute;
   let charactersToEliminate = this.getCharactersToEliminate(relatedKey, attribute);
@@ -43,7 +49,7 @@ GuessWho.prototype.getResult = function (questionId) {
 GuessWho.prototype.getCharactersToEliminate = function (relatedKey, attribute) {
   const charactersToEliminate = [];
   const characters = this.characters;
-  for (character of characters){
+  for (let character in characters){
     if (character.attribute !== this.hiddenCharacter.attribute) {
       charactersToEliminate.push(character);
     };
@@ -51,21 +57,21 @@ GuessWho.prototype.getCharactersToEliminate = function (relatedKey, attribute) {
   return charactersToEliminate;
 };
 
-GuessWho.prototype.getHiddenCharacter = function(characters) {
-  let hiddenCharacter = characters[Math.floor(Math.random()*characters.length)];
+GuessWho.prototype.getHiddenCharacter = function() {
+  let hiddenCharacter = this.characters[Math.floor(Math.random()*this.characters.length)];
   return hiddenCharacter;
 };
 
-GuessWho.prototype.getSelectedQuestion = function (questionId) {
-  const selectedQuestion = null;
+GuessWho.prototype.getSelectedQuestion = function (questionContent) {
   const questions = this.allQuestions;
-
-  for (let question of questions) {
-    if (questionId === question.id){
+  for (let question in questions) {
+    let selectedQuestion = [];
+    if (questionContent === question.question){
       selectedQuestion = question;
     };
+    return selectedQuestion;
   };
-  return selectedQuestion;
+
 };
 
 GuessWho.prototype.updateCards = function (charactersToEliminate) {
