121b467e95cf0b090de3c643f44f4da9d04e7efe
DaibhidhBoyle <Daibhidh_Boyle@gmail.com>
2019-04-24 10:46:33 +0100

    seeds now reflect e29

--- client/src/views/game_result_view.js
+++ client/src/views/game_result_view.js
@@ -9,6 +9,10 @@ GameResultView.prototype.bindEvents = function () {
     const resultedCard = evt.detail;
     this.renderResult(resultedCard);
   });
+  //PubSub.subscribe('Questions:turn-limit', (evt) => {
+    //const turnLimit = evt.detail;
+    //this.renderTurnLimit
+  //});
 };
 
 GameResultView.prototype.renderResult = function (guessedCard) {
